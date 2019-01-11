2249cd04b5d056e111581eedf37666743b0ac5e0
Freddie Ridell <contact@freddieridell.com>
2019-01-11 21:29:31 +0000

    added hyper warm component, and some warning information

--- src/index.js
+++ src/index.js
@@ -2,10 +2,37 @@ import hyperdb from "hyperdb";
 import ram from "random-access-memory";
 import net from "net";
 import portfinder from "portfinder";
+import { spawn } from "child_process";
+import hyperswarm from "@hyperswarm/network";
 
 const announcePort = 12237 || process.env.HYPER_BUDDY_PORT;
 
 const dbs = {};
+
+const createNewDb = async inputKeyBuffer => {
+	const dbKeyString = inputKeyBuffer.toString("base64");
+
+	const db = await new Promise(done => {
+		const db = hyperdb(ram, inputKeyBuffer);
+
+		db.on("ready", () => {
+			dbs[dbKeyString] = db;
+			done(db);
+		});
+	});
+
+	const swarm = hyperswarm({ ephemeral: false });
+	swarm.join(db.discoveryKey, {
+		lookup: true,
+		announce: true,
+	});
+	swarm.on("connection", (socket, details) => {
+		const stream = db.replicate({ live: true });
+		stream.pipe(socket).pipe(stream);
+	});
+
+	return db;
+};
 
 const onReplicateConnection = (
 	inputKeyBuffer,
@@ -13,18 +40,9 @@ const onReplicateConnection = (
 ) => async replicateConnection => {
 	const dbKeyString = inputKeyBuffer.toString("base64");
 
-	const db = await new Promise(done => {
-		if (dbs[dbKeyString]) {
-			done(dbs[dbKeyString]);
-		} else {
-			const db = hyperdb(ram, inputKeyBuffer);
-
-			db.on("ready", () => {
-				dbs[dbKeyString] = db;
-				done(db);
-			});
-		}
-	});
+	const db = await Promise.resolve(
+		dbs[dbKeyString] || createNewDb(inputKeyBuffer),
+	);
 
 	const stream = db.replicate({ live: false });
 	stream.pipe(replicateConnection).pipe(stream);
@@ -64,8 +82,47 @@ export const main = () =>
 			});
 	});
 
+//autorun main if this file is called as an executable
 if (require.main === module) {
-	main();
+	if (process.argv[2] === "forever") {
+		main();
+	} else {
+		console.log("spawning hyperbuddy to live forever!!!");
+		spawn("node", [require.main.filename, "forever"], {
+			detached: true,
+		});
+		process.exit(0);
+	}
 }
 
-export const connectToBuddy = async publicKeyBuffer => {};
+export const connectToBuddy = async publicKeyBuffer => {
+	const createSocketConnectedToPort = port =>
+		new Promise((done, fail) => {
+			const client = new net.Socket();
+			client.on("error", fail);
+			client.connect(
+				port,
+				"localhost",
+				(err, dat) => (err ? fail(err) : done(client)),
+			);
+		});
+
+	try {
+		const announceSocket = await createSocketConnectedToPort(announcePort);
+
+		const replicatePort = await new Promise(done => {
+			announceSocket.on("data", buf =>
+				done(parseInt(buf.toString(), 10)),
+			);
+			announceSocket.write(publicKeyBuffer);
+		});
+
+		return createSocketConnectedToPort(replicatePort);
+	} catch (e) {
+		console.log(
+			`it seems that hyperbuddy is not running, please install @hypercortex/hyperbuddy and run hyperbuddy
+(Hyperbuddy will auto start in later versions)`,
+		);
+		process.exit(1);
+	}
+};
